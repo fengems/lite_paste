6f5d5d9473a50f2137ad75ec0abafccc0435a94f
fenge <fengems@outlook.com>
2026-05-30 08:53:43 +0800

    chore: 发布 0.1.1

--- Sources/LitePasteCore/AppMetadata.swift
+++ Sources/LitePasteCore/AppMetadata.swift
@@ -3,7 +3,7 @@ import Foundation
 public enum AppMetadata {
   public static let displayName = "Lite Paste"
   public static let bundleIdentifier = "com.fengems.LitePaste"
-  public static let version = "0.1.0"
+  public static let version = "0.1.1"
   public static let build = "1"
   public static let minimumMacOSVersion = "15.0"
   public static let licenseName = "Private / All rights reserved"
